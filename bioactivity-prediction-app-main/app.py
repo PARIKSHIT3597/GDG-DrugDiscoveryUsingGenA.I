d67655de37466510c3aaf14635da96ed2d16f40c
PARIKSHIT3597 <143903658+PARIKSHIT3597@users.noreply.github.com>
2025-04-06 17:43:49 +0530

    Update app.py

--- bioactivity-prediction-app-main/app.py
+++ bioactivity-prediction-app-main/app.py
@@ -1,88 +1,98 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
 from PIL import Image
-import subprocess
-import os
 import base64
 import pickle
+import os
 
-# Molecular descriptor calculator
-def desc_calc():
-    # Performs the descriptor calculation
-    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    os.remove('molecule.smi')
+# RDKit-based descriptor calculator (Morgan Fingerprints)
+def desc_calc_rdkit(smiles_list, mol_names):
+    mols = [Chem.MolFromSmiles(smile) for smile in smiles_list]
+    fingerprints = [
+        AllChem.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=2048) if mol else None
+        for mol in mols
+    ]
+    valid_data = [(name, fp) for name, fp in zip(mol_names, fingerprints) if fp is not None]
+    if not valid_data:
+        return None
+    names, fps = zip(*valid_data)
+    arr = np.array([list(fp.ToBitString()) for fp in fps], dtype=int)
+    df = pd.DataFrame(arr)
+    df.insert(0, 'molecule_name', names)
+    df.to_csv('descriptors_output.csv', index=False)
+    return df
 
 # File download
 def filedownload(df):
     csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
     return href
 
-# Model building
-def build_model(input_data):
-    # Reads in saved regression model
-    load_model = pickle.load(open('acetylcholinesterase_model.pkl', 'rb'))
-    # Apply model to make predictions
-    prediction = load_model.predict(input_data)
-    st.header('**Prediction output**')
+# Model prediction
+def build_model(input_data, molecule_names):
+    model = pickle.load(open('acetylcholinesterase_model.pkl', 'rb'))
+    prediction = model.predict(input_data)
+    st.header('**Prediction Output**')
     prediction_output = pd.Series(prediction, name='pIC50')
-    molecule_name = pd.Series(load_data[1], name='molecule_name')
+    molecule_name = pd.Series(molecule_names, name='molecule_name')
     df = pd.concat([molecule_name, prediction_output], axis=1)
     st.write(df)
     st.markdown(filedownload(df), unsafe_allow_html=True)
 
-# Logo image (Cloud-safe)
-script_dir = os.path.dirname(os.path.abspath(__file__))
-logo_path = os.path.join(script_dir, "logo.png")
-image = Image.open(logo_path)
-st.image(image, use_column_width=True)
+# Load and display logo (optional)
+if os.path.exists("logo.png"):
+    image = Image.open('logo.png')
+    st.image(image, use_container_width=True)
 
-# Page title
+# Title
 st.markdown("""
 # Bioactivity Prediction App (Acetylcholinesterase)
 
-This app allows you to predict the bioactivity towards inhibiting the `Acetylcholinesterase` enzyme. `Acetylcholinesterase` is a drug target for Alzheimer's disease.
+This app predicts bioactivity towards inhibiting the **Acetylcholinesterase** enzyme.  
+A key target in drug discovery for Alzheimer's treatment.
 
-**Credits**
-- App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
-- Descriptor calculated using [PaDEL-Descriptor](http://www.yapcwsoft.com/dd/padeldescriptor/) [[Read the Paper]](https://doi.org/10.1002/jcc.21707).
 ---
 """)
 
 # Sidebar
-with st.sidebar.header('1. Upload your CSV data'):
+with st.sidebar.header('1. Upload your SMILES file (.txt)'):
     uploaded_file = st.sidebar.file_uploader("Upload your input file", type=['txt'])
     st.sidebar.markdown("""
 [Example input file](https://raw.githubusercontent.com/dataprofessor/bioactivity-prediction-app/main/example_acetylcholinesterase.txt)
 """)
 
+# Prediction
 if st.sidebar.button('Predict'):
-    load_data = pd.read_table(uploaded_file, sep=' ', header=None)
-    load_data.to_csv('molecule.smi', sep='\t', header=False, index=False)
+    if uploaded_file is not None:
+        load_data = pd.read_table(uploaded_file, sep=' ', header=None)
+        smiles_list = load_data[0].tolist()
+        molecule_names = load_data[1].tolist()
 
-    st.header('**Original input data**')
-    st.write(load_data)
+        st.header('**Original Input Data**')
+        st.write(load_data)
 
-    with st.spinner("Calculating descriptors..."):
-        desc_calc()
+        with st.spinner("Calculating descriptors..."):
+            desc = desc_calc_rdkit(smiles_list, molecule_names)
 
-    # Read in calculated descriptors and display the dataframe
-    st.header('**Calculated molecular descriptors**')
-    desc = pd.read_csv('descriptors_output.csv')
-    st.write(desc)
-    st.write(desc.shape)
+        if desc is None:
+            st.error("Descriptor calculation failed. Please check your SMILES strings.")
+        else:
+            st.header('**Calculated Molecular Descriptors**')
+            st.write(desc)
+            st.write(desc.shape)
 
-    # Read descriptor list used in previously built model
-    st.header('**Subset of descriptors from previously built models**')
-    Xlist = list(pd.read_csv('descriptor_list.csv').columns)
-    desc_subset = desc[Xlist]
-    st.write(desc_subset)
-    st.write(desc_subset.shape)
+            st.header('**Subset of Descriptors Used in Model**')
+            Xlist = list(pd.read_csv('descriptor_list.csv').columns)
+            desc_subset = desc[Xlist]
+            st.write(desc_subset)
+            st.write(desc_subset.shape)
 
-    # Apply trained model to make prediction on query compounds
-    build_model(desc_subset)
+            build_model(desc_subset, desc['molecule_name'])
+    else:
+        st.warning('Please upload a file to continue.')
 else:
-    st.info('Upload input data in the sidebar to start!')
+    st.info('Upload input data in the sidebar to start.')
